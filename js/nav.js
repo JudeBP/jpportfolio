13fb93a4feb5ade9c3a1a78d17ac71a0060d7401
JudeBP <88365957+JudeBP@users.noreply.github.com>
2021-08-10 15:27:51 +0800

    Final Version

--- js/nav.js
+++ js/nav.js
@@ -14,3 +14,45 @@ toggleBtn.addEventListener('click', () => {
         toggleBtn.style.transform = "rotate(0deg)";
     }
 })
+
+
+window.addEventListener('scroll', () => {
+    let scrollHeight = window.pageYOffset;
+    let headerHeight = header.getBoundingClientRect().height;
+
+    if (scrollHeight > headerHeight) {
+        header.classList.add('fixed-header')
+    } else {
+        header.classList.remove('fixed-header')
+    }
+})
+
+const scrollBtns = document.querySelectorAll('.scroll');
+scrollBtns.forEach((btn) => {
+    // let id = btn
+    btn.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        const id = ev.currentTarget.getAttribute("href").slice(1)
+        const element = document.getElementById(id);
+
+        const navHeight = header.getBoundingClientRect().height;
+        const linksHeight = links.getBoundingClientRect().height;
+        const fixedNav = header.classList.contains('fixed-header');
+        let position = element.offsetTop - navHeight;
+
+        if (!fixedNav) {
+            position -= navHeight;
+        }
+        if (navHeight > 82) {
+            position += linksHeight;
+        }
+
+        // console.log(fixedNav)
+        window.scrollTo({
+            left: 0,
+            top: position
+        });
+        links.style.height = '0';
+        toggleBtn.style.transform = "rotate(0deg)";
+    })
+})
